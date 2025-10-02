7e3514e08730357041d541cfa745b6ed270e6248
D58-rm1 <diegoramirezn9@gmail.com>
2025-10-02 13:03:10 -0600

    11

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ var app = express();
 var server = app.listen(4000, function(){
     console.log('Servidor corriendo en http://localhost:4000');
 });
+// Configurar el servidor para escuchar en todas las interfaces de red
+const PORT = 4000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
+
 
 app.use(express.static('public'));
 
-// ⬇️ MODIFICACIÓN CRÍTICA: INCLUIR CORS PARA PERMITIR LA CONEXIÓN DESDE FIREBASE ⬇️
+// ⬇️ MODIFICACIÓN CRÍTICA: INCLUIR CORS PARA PERMITIR LA CONEXIÓN DESDE AWS
 var io = socket(server, {
     cors: {
         
-        origin: "https://chat-alexis-703.web.app", 
+        origin: "https://localhost:4000", 
         methods: ["GET", "POST"]
     }
 });
